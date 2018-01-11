edc5f1c16de7f0c8250f51b2dcf4079f2f428f17
Vitaliy <Vitalliyshevchyk336@gmail.com>
2018-01-11 23:30:56 +0200

    Tabs

---
+++ app/src/main/java/com/example/vitaliy/news/AllNewsFragment.java
@@ -0,0 +1,26 @@
+package com.example.vitaliy.news;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Vitaliy on 1/11/2018.
+ */
+
+public class AllNewsFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.all_news_fragment, container, false);
+    }
+}
