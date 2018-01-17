5dce9662aae9630d9e3023dba1bd87a7947f719a
v_shevchyk <Vitaliyshevchyk336@gmail.com>
2018-01-17 11:55:05 +0200

    improve adapter and adapter items

--- app/src/main/java/com/example/vitaliy/news/ui/adapters/NewsAdapter.java
+++ app/src/main/java/com/example/vitaliy/news/ui/adapters/NewsAdapter.java
@@ -1,6 +1,7 @@
 package com.example.vitaliy.news.ui.adapters;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
@@ -14,6 +15,7 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.example.vitaliy.news.R;
 import com.example.vitaliy.news.data.model.Article;
+import com.example.vitaliy.news.ui.topnews.TopNewsPresenter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,7 +24,7 @@ import java.util.List;
  * Created by v_shevchyk on 15.01.18.
  */
 
-public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder>{
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
     private Context mContext;
     List<Article> newsList = new ArrayList<>();
 
@@ -50,10 +52,12 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder>{
         Log.e("newsAdapter", "news size = "+news.size());
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         CardView cv;
         TextView title, description;
         ImageView newsPicture;
+        TopNewsPresenter presenter;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -65,12 +69,20 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder>{
 
         void bind(Article news) {
             title.setText(news.getTitle());
+            title.setTypeface(null, Typeface.BOLD);
             description.setText(news.getDescription());
+            description.setTypeface(null, Typeface.ITALIC);
             Glide.with(mContext)
                     .load(news.getUrlToImage())
                     .override(300, 150)
+                    .error(R.drawable.n_f)
                     .centerCrop()
                     .into(newsPicture);
+        }
+
+        @Override
+        public void onClick(View view) {
+
         }
     }
 }
