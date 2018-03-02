9511229e431ca8c9235b90a00d558cf6e172cc80
v_shevchyk <Vitaliyshevchyk336@gmail.com>
2018-03-02 15:47:40 +0200

    splash

--- app/src/main/java/com/example/vitaliy/news/ui/splash/SplashActivity.java
+++ app/src/main/java/com/example/vitaliy/news/ui/splash/SplashActivity.java
@@ -20,7 +20,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         animate();
         new Handler().postDelayed(new Runnable() {
             @Override
